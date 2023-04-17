c4283420cd470c44acaf07e8bc86fcc8c35272fc
salman80561 <128211065+salman80561@users.noreply.github.com>
2023-04-18 00:24:23 +0530

    Add files via upload

---
+++ ExpenseItemdel.js
@@ -0,0 +1,23 @@
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
+
+const ExpenseItem =(props) =>{
+  const clickHandler=() => {
+    console.log('Deleted!!!');
+  }
+ 
+  
+  return (
+    <div className='expense-item'>
+        <ExpenseDate date ={props.date}/>
+        <div className='expense-item__description'>
+            <h2>{props.title}</h2>
+            <div className='expense-item__price'>${props.amount}</div>
+            <div className='expense-item__location'>{props.location}</div>
+        </div>
+        <button onClick ={clickHandler}>Delete</button>
+    </div>
+  );
+}
+
+export default ExpenseItem;
