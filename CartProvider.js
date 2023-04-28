d617251ccee5241ea3040d99527d4a51e940609a
salman80561 <128211065+salman80561@users.noreply.github.com>
2023-04-28 21:15:00 +0530

    Add files via upload

--- CartProvider.js
+++ CartProvider.js
@@ -1,8 +1,31 @@
 import CartContext from './cart-context';
-const CartProvider = props =>{
-    const addItemToCartHandler = item => {};
+import {useReducer }  from 'react';
 
-    const removeItemFromCartHandler = id => {};
+const defaultCartState ={
+    items:[],
+    totalAmount : 0
+}
+
+const cartReducer =(state,action)  => {
+    if (action.type === 'ADD') {
+        const updatedItems =state.items.concat(action.item);
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount
+        return{
+            items:updatedItems,
+            totalAmount: updatedTotalAmount
+        }
+    }
+   return defaultCartState;
+};
+const CartProvider = props =>{
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+    const addItemToCartHandler = (item) => {
+        dispatchCartAction({type:'ADD', item:item})
+    };
+
+    const removeItemFromCartHandler = (id) => {
+        dispatchCartAction({type: 'REMOVE', id:id})
+    };
 
     const cartContext = {
         items: [],
