ea6d452df9a2d45ccfb9c1f54fb1dd01ab46140f
salman80561 <128211065+salman80561@users.noreply.github.com>
2023-05-06 23:05:49 +0530

    Add files via upload

---
+++ Product.js
@@ -0,0 +1,80 @@
+import React, { useContext } from "react"
+import { Row, Container, Col, Button } from "react-bootstrap";
+import ProductList from "./ProductList";
+import Econtext from "../store/ecom-context";
+
+const Product = (props) => {
+    const ctx = useContext(Econtext)
+    const productsArr = [
+
+        {
+            id: 'e1',
+            title: 'Colors',
+
+            price: 100,
+
+            imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
+
+        },
+
+        {
+            id: 'e2',
+            title: 'Black and white Colors',
+
+            price: 50,
+
+            imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
+
+        },
+
+        {
+            id: 'e3',
+            title: 'Yellow and Black Colors',
+
+            price: 70,
+
+            imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
+
+        },
+
+        {
+            id: 'e4',
+            title: 'Blue Color',
+
+            price: 100,
+
+            imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%204.png',
+
+        }
+
+    ]
+    
+    const CartShowHandler = () => {
+        (`/Login/Cart/${ctx.token}`)
+        ctx.onShowCart()
+    }
+
+    return (
+        <>
+            <Container className="p-4 text-center h1">
+                PRODUCTS
+            </Container>
+            <Container>
+                <Row className="d-flex justify-content-center" style={{ gridColumnGap: '200px', gridRowGap: '100px' }}>
+                    {productsArr.map((item) => (
+                        <Col lg={3} md={12}
+                            key={item.id} >
+                            <ProductList id={item.id} title={item.title} price={item.price} imageUrl={item.imageUrl} />
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
+
+            <Container className="mb-5 p-4 text-center">
+                <Button variant="secondary" style={{ color: '#56CCF2' }} onClick={CartShowHandler} >See the cart</Button>
+            </Container>
+
+        </>
+    )
+}
+export default Product;
