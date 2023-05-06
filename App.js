89136ccd5efef053e4ac49162881bbf38905071a
salman80561 <128211065+salman80561@users.noreply.github.com>
2023-05-06 23:03:36 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,39 +1,22 @@
-import React, {  useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Product from './Components/Product';
-import Cart from './Components/Cart';
+import React, {Fragment}from "react";
+import Header from "./Layout/Header";
+import Footer from "./Layout/Footer";
+import Product from "./Product/Product";
+
 
 
 function App() {
-  const [cartItems, setCartItems] = useState([]);
-
-  const handleAddToCart = (product) => {
-    setCartItems([...cartItems, product]);
-  };
-
   return (
-    <Router>
-      <div className="App">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Product</Link>
-            </li>
-            <li>
-              <Link to="/cart">Cart ({cartItems.length})</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <div>
-          <Routes>
-            <Route path="/cart" element={<Cart cartItems={cartItems} setCartItems={setCartItems} />} />
-            <Route path="/" element={<Product handleAddToCart={handleAddToCart} />} />
-          </Routes>
-        </div>
-        
-      </div>
-    </Router>
+    <Fragment>
+       <Header/>
+       <Product />
+       <Footer />
+       
+    </Fragment>
+      
+      
+    
+    
   );
 }
 
