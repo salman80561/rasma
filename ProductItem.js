890815e82473cf4c79e05de5f23de92bd3881483
salman80561 <128211065+salman80561@users.noreply.github.com>
2023-05-12 21:04:00 +0530

    Add files via upload

---
+++ ProductItem.js
@@ -0,0 +1,32 @@
+import React, { useContext } from "react";
+import { Button, Card, Row, Col } from "react-bootstrap";
+import CartContext from "../../store/CartContext";
+import { Link } from "react-router-dom";
+
+const ProductItem = (props) => {
+  const ctx = useContext(CartContext);
+  const AddItemHandler = (e) => {
+    ctx.addItem({ ...props, quantity: 1 });
+  };
+  return (
+    <Row style={{ margin: "1rem" }}>
+      <Col>
+        <Card>
+          <Link to={`Store/${props.id}`}>
+          <Card.Img variant="top" src={props.imageUrl} alt={props.title} />
+          </Link>
+          <Card.Body>
+            <Card.Title>{props.title}</Card.Title>
+            <Card.Subtitle>
+              <span>${props.price}</span>
+            </Card.Subtitle>
+            <Button onClick={AddItemHandler} variant="outline-success">
+              Add to Cart
+            </Button>{" "}
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  );
+};
+export default ProductItem;
