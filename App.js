b038ca9bdebacdd65eb73a29101697270e3b2d6c
salman80561 <128211065+salman80561@users.noreply.github.com>
2023-05-13 23:15:20 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,107 +1,101 @@
-import { BrowserRouter as Router, Routes, Navigate, Route } from 'react-router-dom';
-import './App.css';
-import Home from './Home/Home';
-import Root from './MainNavigation/Root';
-import { lazy, Suspense, useContext } from 'react';
-import Econtext from './Store/ecom-context';
-import Header from './Layout/Header';
-import Footer from './Layout/Footer';
-import { Container } from 'react-bootstrap';
-const NotFound = lazy(() => import('./NotFound/NotFound'));
-const SignupForm = lazy(() => import('./SignUp/SignUpForm'));
-const Account = lazy(() => import('./UserAccount/Account'));
-const About = lazy(() => import('./About/About'));
-const Product = lazy(() => import('./Product/Product'));
-const ContactUs = lazy(() => import('./Contact/Contact'));
-const Login = lazy(() => import('./Login/Login'));
-const ProductDetailsPage = lazy(() => import('./Product/ProductDetailsPage'))
-const Cart = lazy(() => import('./Cart/Cart'))
+import React, { useState, useEffect } from 'react';
+import Candy from './Candy';
+import Cart from './Cart';
 
 const App = () => {
-  const ctx = useContext(Econtext);
+  const [candies, setCandies] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [amount, setAmount] = useState('');
+
+  useEffect(() => {
+    // Fetch candies from the API
+    fetchCandies();
+  }, []);
+
+  const fetchCandies = async () => {
+    try {
+      const response = await fetch('');
+      const data = await response.json();
+      setCandies(data);
+    } catch (error) {
+      console.error('Error fetching candies:', error);
+    }
+  };
+
+  const addToCart = (candy) => {
+    const updatedCartItems = [...cartItems, candy];
+    setCartItems(updatedCartItems);
+
+    // Store updated cart items in local storage
+    localStorage.setItem('cartItems', JSON.stringify(updatedCartItems));
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    const newCandy = { name, description, amount };
+
+    // Add the new candy to the API
+    addCandy(newCandy);
+
+    // Clear the input fields
+    setName('');
+    setDescription('');
+    setAmount('');
+  };
+
+  const addCandy = async (candy) => {
+    try {
+      const response = await fetch('https://crudcrud.com/api/e5e4ba1e77d84ec5be5a66b863737619/candies', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(candy),
+      });
+      const data = await response.json();
+
+      // Update the candies state with the new candy
+      setCandies([...candies, data]);
+    } catch (error) {
+      console.error('Error adding candy:', error);
+    }
+  };
+
   return (
     <div>
-      
-      <Container className='overflow-auto h-100 p-0' fluid>
-        <Suspense fallback={<p>Loading...</p>}>
-          <Root>
-            <Routes>
-              <Route path='/' element={<Navigate to='/home' replace />} />
-              <Route path='/home' element={
-                <>
-                  <Header />
-                  <Home />
-                  <Footer />
-                </>
-              } />
-              {ctx.isLogedin && <Route path='/Login/Product/:email' element={
-                <>
-                  <Header />
-                  <Product />
-                  <Footer />
-                </>
-              } />}
-              {ctx.isLogedin && <Route path='/Product/:id' element={
-                <>
-                  <Header />
-                  <ProductDetailsPage />
-                  <Footer />
-                </>
-              } />}
-              {ctx.isLogedin && <Route path='/Login/Cart/:userId' element={
-                <>
-                  <Header />
-                  <Cart />
-                  <Footer />
-                </>
-              } />}
-              <Route path='/About' element={
-                <>
-                  <Header />
-                  <About />
-                  <Footer />
-                </>
-              } />
-              
-             
-              <Route path='/Contact' element={
-                <>
-                  <Header />
-                  <ContactUs />
-                  <Footer />
-                </>
-              } />
-              <Route path='/Login' element={
-                <>
-                  <Header />
-                  <Login />
-                  <Footer />
-                </>
-              } />
-              <Route path='/SignUp' element={
-                <>
-                  <Header />
-                  <SignupForm />
-                  <Footer />
-                </>
-              } />
-              
-              {ctx.isLogedin && <Route path='/Login/:emailId' element={
-                <>
-                  <Header />
-                  <Account />
-                  <Footer />
-                </>
-              } />}
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </Root>
-        </Suspense>
-      </Container>
-      
+      <h1>Salman Candy Shop</h1>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          placeholder="Candy Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Amount"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        <button type="submit">Add Candy</button>
+      </form>
+      <div>
+        <h2>Candies</h2>
+        {candies.map((candy, index) => (
+          <Candy key={index} candy={candy} onAddToCart={addToCart} />
+        ))}
+      </div>
+      <Cart cartItems={cartItems} />
     </div>
-    
   );
-}
+};
 
 export default App;
